5d4e461e0af4a83a306f336088d24f26efbcb750
Jose <oladotjos@gmail.com>
2025-08-30 08:51:20 +0100

    text-done

--- src/app/Components.tsx
+++ src/app/Components.tsx
@@ -168,11 +168,14 @@ export function AudioGearSection() {
     className="object-cover h-100 border rounded-lg"
     />
 
-     <h1 className="font-bold text-xl"> BRINGING YOU THE <br/> <span className="text-BurntSienna-100">BEST</span> AUDIO GEAR</h1>            
-      <p className="text-xs leading-5">  Located at the heart of New York City, Audiophile is the premier store for high end headphones, earphones, speakers, and audio accessories.
+    <div className='md:w-3/4 md:mx-auto md:flex md:flex-col md:items-center md:space-y-6'>
+    <h1 className="font-bold text-xl md:w-4/5 md:text-3xl"> BRINGING YOU THE  <span className="text-BurntSienna-100">BEST</span> AUDIO GEAR</h1>            
+      <p className="text-xs md:text-sm md:leading-6 leading-5 md:text-Gray-200">  Located at the heart of New York City, Audiophile is the premier store for high end headphones, earphones, speakers, and audio accessories.
         We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some
          of the fantastic people who make Audiophile the best place to buy your portable audio equipment.
       </p>  
+    </div>
+     
   </section>
   )
 
