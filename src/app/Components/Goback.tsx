7998c79091b8777a29468f861d2108282115294d
Jose <oladotjos@gmail.com>
2025-09-02 15:24:49 +0100

    UI-desktop-checkoutPage-dONE

--- src/app/Components/Goback.tsx
+++ src/app/Components/Goback.tsx
@@ -1,8 +1,12 @@
 "use client";
 import { useRouter } from "next/navigation";
 
+interface GobackProps {
+  className?: string;
+}
 
-export function Goback() {
+
+export function Goback({ className }: GobackProps) {
     const router=useRouter();
     function handleBackNavigation(){
       if (typeof window !== 'undefined' && window.history.length > 1) {
@@ -13,7 +17,7 @@ export function Goback() {
       }
     }
     return (
-      <button onClick={handleBackNavigation} className="text-xs md:text-sm my-2 md:mb-6 lg:mb-11 text-Gray-200 hover:text-BurntSienna-100">
+      <button onClick={handleBackNavigation} className={`text-xs md:text-sm my-2 md:mb-6  ${className} text-Gray-200 hover:text-BurntSienna-100`}>
         Go Back
       </button>
     );
