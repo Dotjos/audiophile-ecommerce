c8efee0e56fe16906c0644d5b704cd75df00f831
Jose <oladotjos@gmail.com>
2025-09-03 17:42:07 +0100

    --mobile-cart-Done

--- src/app/Components/Cart.tsx
+++ src/app/Components/Cart.tsx
@@ -13,20 +13,22 @@ const Cart = () => {
 
   return (
     <Backdrop className="w-full flex justify-center md:justify-end md:pr-10 pt-16 md:pt-24">
-    <div className=" w-9/10 md:w-1/2 lg:w-1/3  rounded-md p-5 top-4 absolute bg-PureWhite-100">
+    <div className=" w-9/10 md:w-1/2 lg:w-1/3 h-ato rounded-md p-5 top-4 absolute bg-PureWhite-100">
         <div className="flex justify-between mb-7">
             <h1 className="font-bold md:text-base text-md">CART({totalItems})</h1>
             <button className="text-sm md:text-base text-Gray-200 underline" onClick={clearCart}>Remove all</button>
         </div>
 
         {/* Cart Items */}
+        <div className="max-h-50 overflow-y-auto">
         {cartItems.length === 0 ? (
           <p className="text-center md:text-base text-Gray-200">Your cart is empty</p>
         ) : (
-          cartItems.map((item) => (
+            cartItems.map((item) => (
             <CartItem item={item} key={item.id}/>
-          ))
+          ))   
         )}
+        </div>
         <div className="flex justify-between mt-4 md:my-7">
           <h1 className="text-sm md:text-base text-Gray-200">TOTAL</h1>
           <h2 className="font-bold md:text-base">{formatPrice(totalPrice)}</h2>
